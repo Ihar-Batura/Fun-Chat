dffd69070293a124569f47d46f4c45a309186015
Ihar-Batura <A17331582@gmail.com>
2025-04-02 16:26:30 +0300

    refactor: change function createForm

---
+++ src/ui/form.ts
@@ -0,0 +1,52 @@
+import createElement from '../create/create_element';
+import createFormLineElement from '../components/form_line_element';
+import createButton from '../create/create_button';
+
+function createForm(): HTMLElement {
+  const form: HTMLElement = createElement({ tag: 'form', classes: ['form', 'login-form'] });
+  form.setAttribute('autocomplete', 'off');
+
+  createElement({
+    tag: 'h2',
+    classes: ['form-title'],
+    text: 'Authentication',
+    parent: form,
+  });
+
+  createFormLineElement({
+    parent: form,
+    labelText: 'Login',
+    inputId: 'login',
+    inputType: 'text',
+    inputTitle:
+      'Enter Login (3-25 latin letters and the hyphen (-) symbol, first letter uppercase)',
+    inputMinLength: '3',
+    pattern: '[A-Z]{1}[a-z\\-]{2,25}',
+    inputRules: 'login-rules',
+    spanId: 'login-rules',
+    spanText: 'Minimum 3 characters, the first letter is in uppercase',
+  });
+
+  createFormLineElement({
+    parent: form,
+    labelText: 'Password',
+    inputId: 'password',
+    inputType: 'password',
+    inputTitle: 'Enter Password (6-25 latin letters, numbers and the hyphen (-) symbol)',
+    inputMinLength: '6',
+    pattern: '[A-Za-z0-9\\-]{6,25}',
+    inputRules: 'password-rules',
+    spanId: 'password-rules',
+    spanText: 'Minimum 6 characters, only letters, numbers and the hyphen (-) symbol',
+  });
+
+  createButton({
+    classes: ['btn', 'form-btn'],
+    text: 'Submit',
+    parent: form,
+  });
+
+  return form;
+}
+
+export default createForm;
