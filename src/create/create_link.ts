4059efddc38ab18036c5a95f42cccd4d577090c9
Ihar-Batura <A17331582@gmail.com>
2025-04-02 11:00:14 +0300

    feat: add function createLink

---
+++ src/create/create_link.ts
@@ -0,0 +1,26 @@
+import { Link } from '../types/types';
+
+function createLink({ href, classes, id, text, target, parent }: Link): HTMLAnchorElement {
+  const link: HTMLAnchorElement = document.createElement('a');
+  link.href = href;
+
+  if (classes) {
+    link.classList.add(...classes);
+  }
+  if (id) {
+    link.id = id;
+  }
+  if (text) {
+    link.textContent = text;
+  }
+  if (target) {
+    link.setAttribute('target', target);
+  }
+  if (parent) {
+    parent.append(link);
+  }
+
+  return link;
+}
+
+export default createLink;
