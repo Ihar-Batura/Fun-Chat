8529bf5fed6c07ae6dbb0058d86ae301795393aa
Ihar-Batura <A17331582@gmail.com>
2025-04-03 14:51:13 +0300

    feat: add function createPopupNoConnectServer

---
+++ src/popups/no_connect_server.ts
@@ -0,0 +1,35 @@
+import createElement from '../create/create_element';
+
+function createPopupNoConnectServer(): HTMLElement {
+  const popup: HTMLElement = createElement({ tag: 'div', classes: ['popup', 'popup-connect'] });
+
+  const popupTextContainer = createElement({
+    tag: 'div',
+    classes: ['popup-text-container'],
+    parent: popup,
+  });
+
+  createElement({
+    tag: 'div',
+    classes: ['popup-text-title'],
+    text: `The connection to the server isn't open!`,
+    parent: popupTextContainer,
+  });
+
+  createElement({
+    tag: 'div',
+    classes: ['popup-text-title'],
+    text: 'Reconnecting...',
+    parent: popupTextContainer,
+  });
+
+  createElement({
+    tag: 'div',
+    classes: ['popup-img'],
+    parent: popupTextContainer,
+  });
+
+  return popup;
+}
+
+export default createPopupNoConnectServer;
