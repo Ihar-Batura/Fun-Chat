76859820d233a34e46017e229dca0efac908447a
Ihar-Batura <A17331582@gmail.com>
2025-04-03 10:15:08 +0300

    feat: add call function when click header button

--- src/ui/header_btns.ts
+++ src/ui/header_btns.ts
@@ -1,5 +1,7 @@
 import createElement from '../create/create_element';
 import createButton from '../create/create_button';
+import createPageAbout from '../pages/about';
+import clickBtnLoginOrLogout from '../utils/click_btn_login_or_logout';
 
 function createHeaderBtns(): HTMLElement {
   const btnsContainer: HTMLElement = createElement({
@@ -10,12 +12,14 @@ function createHeaderBtns(): HTMLElement {
   createButton({
     classes: ['btn', 'header-btn', 'btn-about'],
     text: 'About',
+    onClick: () => createPageAbout(),
     parent: btnsContainer,
   });
 
   createButton({
     classes: ['btn', 'header-btn', 'btn-login-logout'],
     text: 'LogIn',
+    onClick: () => clickBtnLoginOrLogout(),
     parent: btnsContainer,
   });
 
