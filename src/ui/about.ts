99b63d5c991d7a64eff0e3a4f3abf6c05edae68f
Ihar-Batura <A17331582@gmail.com>
2025-04-03 10:10:55 +0300

    refactor: move function createAbout to other folder

--- src/ui/about.ts
+++ src/ui/about.ts
@@ -1,67 +1,67 @@
-import createElement from '../functional/create/create_element'
-import createButton from '../functional/create/create_button'
+import createElement from '../create/create_element';
+import createButton from '../create/create_button';
 
 function createAbout(): HTMLElement {
-  const aboutContainer = createElement({
+  const aboutContainer: HTMLElement = createElement({
     tag: 'div',
     classes: ['about-container'],
-  })
+  });
 
   createElement({
     tag: 'h2',
-    classes: ['about-container__title'],
+    classes: ['about-container-title'],
     text: 'Welcome',
     parent: aboutContainer,
-  })
+  });
   createElement({
     tag: 'h3',
-    classes: ['about-container__title', 'about-container__title-h3'],
+    classes: ['about-container-title', 'about-container-title-h3'],
     text: 'My dear friend',
     parent: aboutContainer,
-  })
+  });
 
   createElement({
     tag: 'p',
-    classes: ['about-container__description'],
+    classes: ['about-container-description'],
     text: 'This project was created for educational purposes.',
     parent: aboutContainer,
-  })
+  });
 
   createElement({
     tag: 'p',
-    classes: ['about-container__description'],
+    classes: ['about-container-description'],
     text: 'The application is a client for a chat service that operates over WebSocket.',
     parent: aboutContainer,
-  })
+  });
 
   createElement({
     tag: 'p',
-    classes: ['about-container__description'],
+    classes: ['about-container-description'],
     text: 'The client application enables users to send messages, view chat history, and also allows senders to delete and edit messages.',
     parent: aboutContainer,
-  })
+  });
 
   createElement({
     tag: 'p',
-    classes: ['about-container__description'],
+    classes: ['about-container-description'],
     text: 'Please note, chat also requires a separate local server to be running for its functionality. This server handles chat features, message storage, and user management.',
     parent: aboutContainer,
-  })
+  });
 
   createElement({
     tag: 'p',
-    classes: ['about-container__description'],
+    classes: ['about-container-description'],
     text: `Now you can fun chatting with your friends and don't think that someone else read your messages.`,
     parent: aboutContainer,
-  })
+  });
 
   createButton({
-    classes: ['btn', 'about-container__btn-return'],
+    classes: ['btn', 'about-container-btn-return'],
     text: 'Return Back',
     parent: aboutContainer,
-  })
+  });
 
-  return aboutContainer
+  return aboutContainer;
 }
 
-export default createAbout
+export default createAbout;
