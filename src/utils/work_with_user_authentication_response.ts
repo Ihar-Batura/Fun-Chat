b82a6ffdb7c871964a999d2d88f0687a77138ba3
Ihar-Batura <A17331582@gmail.com>
2025-04-08 13:13:00 +0300

    refactor: fix function workWithUserAuthenticationResponse

--- src/utils/work_with_user_authentication_response.ts
+++ src/utils/work_with_user_authentication_response.ts
@@ -1,17 +1,20 @@
-import { UserAuthentication, UserAuthenticationError } from '../types/types';
+import { UserAuthentication, ServerResponseError } from '../types/types';
 import createModalWindow from '../popups/modal_window';
 import { user } from '../constants/variables';
+import createPageMain from '../pages/main';
+import deleteUserLoginAndPasswordFromSessionStorage from '../storage/delete_user_login_and_password_from_SS';
 
 function workWithUserAuthenticationResponse(response: string) {
-  const data: UserAuthentication | UserAuthenticationError = JSON.parse(response);
+  const data: UserAuthentication | ServerResponseError = JSON.parse(response);
 
   if (data.type === 'USER_LOGIN') {
     user.isLogined = true;
-    console.log(user.login, user.isLogined);
+    createPageMain();
   } else if (data.type === 'ERROR') {
     user.isLogined = false;
     const errorText: string = data.payload.error;
     createModalWindow(errorText);
+    deleteUserLoginAndPasswordFromSessionStorage();
   }
 }
 
