3d0c3ff017d7205ae90aef0fd80ca9651ef0a5e1
Ihar-Batura <A17331582@gmail.com>
2025-04-08 13:12:28 +0300

    fix: fix function clickBtnLoginOrLogout

--- src/utils/click_btn_login_or_logout.ts
+++ src/utils/click_btn_login_or_logout.ts
@@ -1,16 +1,15 @@
 import createPageLogin from '../pages/login';
+import addRoutePath from '../routes/add_route_path';
+import sendUserLogoutToServer from '../sockets/user_logout';
 
-function clickBtnLoginOrLogout(): void {
-  const btn: Element | null = document.querySelector('.btn-login-logout');
+function clickBtnLoginOrLogout(btn: HTMLButtonElement): void {
+  const btnText: string | null = btn.textContent;
 
-  if (btn !== null) {
-    const btnText: string | null = btn.textContent;
-
-    if (btnText === 'LogIn') {
-      createPageLogin();
-    } else {
-      console.log('Make LogOut');
-    }
+  if (btnText === 'LogIn') {
+    createPageLogin();
+    addRoutePath('#/login');
+  } else {
+    sendUserLogoutToServer();
   }
 }
 
