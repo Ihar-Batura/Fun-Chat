fd155d7b0777c50436baa16dc600e5a7fd3f0817
Ihar-Batura <A17331582@gmail.com>
2025-04-08 13:03:12 +0300

    feat: add work with router on start page

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,23 @@ import './sass/style.scss';
 import checkPageRoute from './routes/check_page_route';
 import createPageLogin from './pages/login';
 import addRoutePath from './routes/add_route_path';
-import startWebSocket from './sockets';
+import startWebSocket from './sockets/open_WS';
+import { user } from './constants/variables';
+import { User } from './types/types';
+import getUserLoginAndPasswordFromSessionStorage from './storage/get_user_login_and_password_from_SS';
+import createPageMain from './pages/main';
 
-createPageLogin();
-checkPageRoute();
-addRoutePath('#/login');
+const userData: User | null = getUserLoginAndPasswordFromSessionStorage();
+if (userData === null) {
+  createPageLogin();
+  checkPageRoute();
+  addRoutePath('#/login');
+} else {
+  user.login = userData.login;
+  user.password = userData.password;
+  user.isLogined = true;
+  createPageMain();
+  checkPageRoute();
+}
+
 startWebSocket();
