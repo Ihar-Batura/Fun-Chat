f204eef55035796d30204c156d2734d841a87e52
Ihar-Batura <A17331582@gmail.com>
2025-04-10 12:04:20 +0300

    feat: add submit listener to dialogue form

--- src/ui/dialogue_form.ts
+++ src/ui/dialogue_form.ts
@@ -1,11 +1,18 @@
 import createElement from '../create/create_element';
 import createInput from '../create/create_input';
 import createButton from '../create/create_button';
+import sendMessageToUser from '../sockets/send_message_to_user';
 
 function createDialogueForm(): HTMLElement {
   const dialogueForm: HTMLElement = createElement({
     tag: 'form',
     classes: ['user-dialogue-form'],
+  });
+
+  dialogueForm.setAttribute('autocomplete', 'off');
+  dialogueForm.addEventListener('submit', (event: SubmitEvent) => {
+    event.preventDefault();
+    sendMessageToUser();
   });
 
   createInput({
@@ -14,12 +21,14 @@ function createDialogueForm(): HTMLElement {
     required: 'true',
     placeholder: 'Message...',
     minLength: '1',
+    disabled: true,
     parent: dialogueForm,
   });
 
   createButton({
     classes: ['btn', 'user-dialogue-form-button'],
     text: '',
+    disabled: true,
     parent: dialogueForm,
   });
 
