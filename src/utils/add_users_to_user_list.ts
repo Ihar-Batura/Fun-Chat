f0b33f8abe98a3b98703182348792cf8056ba884
Ihar-Batura <A17331582@gmail.com>
2025-04-11 20:46:03 +0300

    feat: add sort for users to function addUsersToUserList

--- src/utils/add_users_to_user_list.ts
+++ src/utils/add_users_to_user_list.ts
@@ -1,23 +1,44 @@
 import { User } from '../types/types';
+import { selectedUser } from '../constants/variables';
 import createUser from '../components/user';
+import selectContact from './select_contact';
+import turnOnDialogueForm from './turn_on_dialogue_form';
+import getMessageHistoryForCheckNotReadMessages from '../sockets/get_message_history_for_check_not_read_messages';
+import searchUserInUserList from './search_user_in_user_list';
 
 function addUsersToUserList(userArray: User[]): void {
   const userList: Element | null = document.querySelector('.user-list-users-container');
   if (userList && userArray.length > 0) {
-    userArray.forEach((el) => {
+    const sortUsers: User[] = userArray.sort((a, b) => a.login.localeCompare(b.login));
+
+    sortUsers.forEach((el) => {
       const userName: string = el.login;
       const userIndicator: boolean | undefined = el.isLogined;
       if (userIndicator !== undefined) {
-        const user = createUser({
-          userIndicator: `${userIndicator === true ? 'online' : 'offline'}`,
+        const userStatus: string = userIndicator === true ? 'online' : 'offline';
+
+        const user: HTMLElement = createUser({
+          userIndicator: `${userStatus}`,
           userName: `${userName}`,
           userMessages: '',
         });
 
+        user.addEventListener('click', () => {
+          selectContact(userName, userIndicator);
+          turnOnDialogueForm();
+        });
+
         userList.append(user);
+        getMessageHistoryForCheckNotReadMessages(userName);
+
+        if (selectedUser.name === userName) {
+          selectContact(userName, userIndicator);
+          turnOnDialogueForm();
+        }
       }
     });
   }
+  searchUserInUserList();
 }
 
 export default addUsersToUserList;
