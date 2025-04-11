b88d060af02777560363884302573d0c943f2157
Ihar-Batura <A17331582@gmail.com>
2025-04-11 20:28:33 +0300

    refactor: fix function searchUserInUserList and make it work better

--- src/utils/search_user_in_user_list.ts
+++ src/utils/search_user_in_user_list.ts
@@ -1,23 +1,28 @@
-function searchUserInUserList(inputValue: string): void {
+function searchUserInUserList(): void {
+  const searchInput: Element | null = document.querySelector('.users-list-search-input');
   const users: NodeListOf<Element> = document.querySelectorAll('.user');
 
-  if (users.length > 0) {
-    if (inputValue.length > 0) {
-      users.forEach((user) => {
-        const userName: string | null = user.children[1].textContent;
-        if (!userName) {
-          return;
-        }
-        if (userName.startsWith(inputValue)) {
+  if (searchInput && searchInput instanceof HTMLInputElement) {
+    const inputValue: string = searchInput.value;
+
+    if (users.length > 0) {
+      if (inputValue.length > 0) {
+        users.forEach((user) => {
+          const userName: string | null = user.children[1].textContent;
+          if (!userName) {
+            return;
+          }
+          if (userName.startsWith(inputValue)) {
+            user.classList.remove('hide');
+          } else {
+            user.classList.add('hide');
+          }
+        });
+      } else {
+        users.forEach((user) => {
           user.classList.remove('hide');
-        } else {
-          user.classList.add('hide');
-        }
-      });
-    } else {
-      users.forEach((user) => {
-        user.classList.remove('hide');
-      });
+        });
+      }
     }
   }
 }
