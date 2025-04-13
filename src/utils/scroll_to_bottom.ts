85a8e9e1584578463563e4e3d9ec9ff35a0fd39e
Ihar-Batura <A17331582@gmail.com>
2025-04-13 12:23:45 +0300

    refactor: union to scroll functions to one

--- src/utils/scroll_to_bottom.ts
+++
@@ -1,8 +0,0 @@
-function scrollToBottom(): void {
-  const element: Element | null = document.querySelector('.user-dialogue-content-container');
-  if (element) {
-    element.scrollTop = element.scrollHeight;
-  }
-}
-
-export default scrollToBottom;
--- src/utils/scroll_to_bottom_or_element.ts
+++ src/utils/scroll_to_bottom_or_element.ts
@@ -1,4 +1,4 @@
-function scrollToElement(): void {
+function scrollToBottomOrElement(): void {
   const container: Element | null = document.querySelector('.user-dialogue-content-container');
   const target: Element | null = document.querySelector('.dialogue-line-container');
 
@@ -7,7 +7,9 @@ function scrollToElement(): void {
       behavior: 'smooth',
       block: 'start',
     });
+  } else if (container) {
+    container.scrollTop = container.scrollHeight;
   }
 }
 
-export default scrollToElement;
+export default scrollToBottomOrElement;
