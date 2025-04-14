824fd467a54d976238fa70043dc82c7f0b123c17
Ihar-Batura <A17331582@gmail.com>
2025-04-14 08:44:56 +0300

    refactor: fix form.ts

--- src/ui/form.ts
+++ src/ui/form.ts
@@ -19,12 +19,12 @@ function createForm(): HTMLElement {
     inputId: 'login',
     inputType: 'text',
     inputTitle:
-      'Enter Login (3-25 latin letters and the hyphen (-) symbol, first letter uppercase)',
+      'Enter Login (3-15 latin letters and the hyphen (-) symbol, first letter uppercase)',
     inputMinLength: '3',
-    pattern: '[A-Z]{1}[a-z\\-]{2,25}',
+    pattern: '[A-Z]{1}[a-z\\-]{2,14}',
     inputRules: 'login-rules',
     spanId: 'login-rules',
-    spanText: 'Minimum 3 characters, the first letter is in uppercase',
+    spanText: 'Minimum 3 letters, the first letter is in uppercase',
   });
 
   createFormLineElement({
@@ -32,9 +32,9 @@ function createForm(): HTMLElement {
     labelText: 'Password',
     inputId: 'password',
     inputType: 'password',
-    inputTitle: 'Enter Password (6-25 latin letters, numbers and the hyphen (-) symbol)',
+    inputTitle: 'Enter Password (6-20 latin letters, numbers and the hyphen (-) symbol)',
     inputMinLength: '6',
-    pattern: '[A-Za-z0-9\\-]{6,25}',
+    pattern: '[A-Za-z0-9\\-]{6,20}',
     inputRules: 'password-rules',
     spanId: 'password-rules',
     spanText: 'Minimum 6 characters, only letters, numbers and the hyphen (-) symbol',
