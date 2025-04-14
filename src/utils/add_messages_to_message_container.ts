1f2a645e529bcc817b2325e0170dae3e92e92966
Ihar-Batura <A17331582@gmail.com>
2025-04-14 08:46:51 +0300

    feat: add rules for add message to message container

--- src/utils/add_messages_to_message_container.ts
+++ src/utils/add_messages_to_message_container.ts
@@ -1,12 +1,31 @@
 import { Message } from '../types/types';
 import createMessage from '../components/message';
+import { isEditMessage, selectedUser, isNeedToAddNewMessageLine } from '../constants/variables';
+import createNewMessageLineElement from '../components/new_message_line';
+import scrollToBottomOrElement from './scroll_to_bottom_or_element';
+
+let isAddNewMessageLine: boolean = false;
 
 function addMessagesToMessageContainer(messageContainer: Element, arrMessages: Message[]): void {
-  console.log(arrMessages);
   arrMessages.forEach((el: Message) => {
+    const conditionsForAddLine: boolean =
+      !el.status.isReaded &&
+      !isAddNewMessageLine &&
+      el.from === selectedUser.name &&
+      isNeedToAddNewMessageLine.value;
+
+    if (conditionsForAddLine) {
+      const lineElement: HTMLElement = createNewMessageLineElement();
+      messageContainer.append(lineElement);
+      isAddNewMessageLine = true;
+    }
     const message: HTMLElement = createMessage(el);
     messageContainer.append(message);
   });
+
+  isEditMessage.value = false;
+  isAddNewMessageLine = false;
+  scrollToBottomOrElement();
 }
 
 export default addMessagesToMessageContainer;
