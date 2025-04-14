a9d06916d7f7798ade4ce584479887adaccc8323
Ihar-Batura <A17331582@gmail.com>
2025-04-14 08:46:13 +0300

    feat: add listener on search input

--- src/ui/users_list_container.ts
+++ src/ui/users_list_container.ts
@@ -1,6 +1,8 @@
 import createElement from '../create/create_element';
 import createInput from '../create/create_input';
-import createUser from '../components/user';
+import getAllAuthenticatedUsersFromServer from '../sockets/get_all_authenticate_users';
+import getAllUnauthorizedUsersFromServer from '../sockets/get_all_unauthorized_users';
+import searchUserInUserList from '../utils/search_user_in_user_list';
 
 function createUsersListContainer(): HTMLElement {
   const container: HTMLElement = createElement({
@@ -12,44 +14,18 @@ function createUsersListContainer(): HTMLElement {
     classes: ['users-list-search-input'],
     type: 'text',
     placeholder: 'Search...',
+    onInput: () => searchUserInUserList(),
     parent: container,
   });
 
-  const usersContainer: HTMLElement = createElement({
+  createElement({
     tag: 'div',
     classes: ['user-list-users-container'],
     parent: container,
   });
 
-  // временно
-  const user1 = createUser({
-    userIndicator: 'online',
-    userName: 'Vitaliy',
-    userMessages: '4',
-  });
-  const user2 = createUser({
-    userIndicator: 'online',
-    userName: 'Tima',
-    userMessages: '1',
-  });
-  const user3 = createUser({
-    userIndicator: 'offline',
-    userName: 'Nasty',
-    userMessages: '11',
-  });
-  const user4 = createUser({
-    userIndicator: 'online',
-    userName: 'Roma',
-    userMessages: '17',
-  });
-  const user5 = createUser({
-    userIndicator: 'offline',
-    userName: 'fegrfegge',
-    userMessages: '',
-  });
-
-  usersContainer.append(user1, user2, user3, user4, user5);
-  // временно
+  getAllAuthenticatedUsersFromServer();
+  getAllUnauthorizedUsersFromServer();
 
   return container;
 }
