021ddb604217d319d952791872d94fceacc6e2d2
Ihar-Batura <A17331582@gmail.com>
2025-04-14 08:50:27 +0300

    refactor: fix function workWithAllUnauthorizedUsersResponse

--- src/utils/work_with_all_unauthorized_users_response.ts
+++ src/utils/work_with_all_unauthorized_users_response.ts
@@ -1,8 +1,6 @@
 import { GetAllUsersResponse, ServerResponseError, User } from '../types/types';
 import createModalWindow from '../popups/modal_window';
 import addUsersToUserList from './add_users_to_user_list';
-import { searchValue } from '../constants/variables';
-import searchUserInUserList from './search_user_in_user_list';
 
 function workWithAllUnauthorizedUsersResponse(response: string): void {
   const data: GetAllUsersResponse | ServerResponseError = JSON.parse(response);
@@ -10,7 +8,6 @@ function workWithAllUnauthorizedUsersResponse(response: string): void {
   if (data.type === 'USER_INACTIVE') {
     const usersArr: User[] = data.payload.users;
     addUsersToUserList(usersArr);
-    searchUserInUserList(searchValue.value);
   } else if (data.type === 'ERROR') {
     const errorText: string = data.payload.error;
     createModalWindow(errorText);
