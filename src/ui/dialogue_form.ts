300fb85ad887596dc7842811f27292542bf70a9f
Ihar-Batura <A17331582@gmail.com>
2025-04-14 08:44:07 +0300

    feat: add cal functions makeMessagesAsRead and tMessageText to function createDialogueForm

--- src/ui/dialogue_form.ts
+++ src/ui/dialogue_form.ts
@@ -2,6 +2,9 @@ import createElement from '../create/create_element';
 import createInput from '../create/create_input';
 import createButton from '../create/create_button';
 import sendMessageToUser from '../sockets/send_message_to_user';
+import { isEditMessage } from '../constants/variables';
+import editMessageText from '../sockets/edit_message_text';
+import makeMessagesAsRead from '../utils/make_messages_as_read';
 
 function createDialogueForm(): HTMLElement {
   const dialogueForm: HTMLElement = createElement({
@@ -10,9 +13,15 @@ function createDialogueForm(): HTMLElement {
   });
 
   dialogueForm.setAttribute('autocomplete', 'off');
+
   dialogueForm.addEventListener('submit', (event: SubmitEvent) => {
     event.preventDefault();
-    sendMessageToUser();
+    if (!isEditMessage.value) {
+      makeMessagesAsRead();
+      sendMessageToUser();
+    } else {
+      editMessageText();
+    }
   });
 
   createInput({
